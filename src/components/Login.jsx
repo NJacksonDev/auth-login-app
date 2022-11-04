34efae2ce5b5e583978bb426a1755f9091c3a226
Nick Jackson <nickdjackson@msn.com>
2022-11-04 19:59:16 -0400

    second

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,39 @@
-import {Button, Form, Input, Layout} from 'antd'
+import { Button, Form, Input, Layout } from 'antd'
 
-export default function Login() {
-    const handleFormSubmit = (values) => {
-        console.log("Success:", values)
-    } 
-    return (
-        <Layout.Content style ={{ padding: '50px'}}>
-            <h1>Login</h1>
-            <Form onFinish={handleFormSubmit} labelCol={{ span: 8}} wrapperCol={{ span: 16 }} >
-                <Form.Item label="Email" name="email" rules={[{
-                    required: true,
-                    message: 'Please enter a valid email address'
-                }]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item label="Password" name="password" rules={[{
-                    required: true,
-                    message: 'Please enter password'
-                }]}>
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
-                    <Button type="primary" htmlType='submit'>Login</Button>
-                </Form.Item>
-            </Form>
-        </Layout.Content>
-    )
+export default function Login({ setUser, setToken }) {
+  const handleFormSubmit = (values) => {
+    fetch('http://localhost:3030/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setToken(data.token)
+        setUser(data.user)
+      })
+      .catch(alert)
+  }
+  return (
+    <Layout.Content style={{ padding: '50px' }}>
+      <h1>Login</h1>
+      <Form onFinish={handleFormSubmit} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        <Form.Item label="Email" name="email" rules={[{
+          required: true,
+          message: 'Please enter a valid email address'
+        }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password" rules={[{
+          required: true,
+          message: 'Please enter password'
+        }]}>
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <Button type="primary" htmlType='submit'>Login</Button>
+        </Form.Item>
+      </Form>
+    </Layout.Content>
+  )
 }
